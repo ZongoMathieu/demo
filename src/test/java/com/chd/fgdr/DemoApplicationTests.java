5b25e6f0b74e3158fa9a26096248c7fae2cd99c3
Mathieu ZONGO <matzongo.ls@gmail.com>
2020-10-18 10:13:49 +0000

    Initial Project

---
+++ src/test/java/com/chd/fgdr/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chd.fgdr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
